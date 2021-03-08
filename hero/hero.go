d3cf19ed443394b3f407830e605f228339f07528
Daniel Urbanski <urbanski@gmail.com>
2021-03-07 18:35:19 -0800

    Update hero structure

--- hero/hero.go
+++ hero/hero.go
@@ -2,7 +2,68 @@ package hero
 
 import (
 	"fmt"
+	"strings"
 )
+
+// The Hero is the main player character.
+type Hero struct {
+	ID          string
+	Name        string
+	Description string
+	HP          Stat
+	Stats       Stats
+	Equipment   Equipment
+}
+
+func (hero Hero) String() string {
+	return fmt.Sprintf("%v - %v | %v | %v", hero.Name, hero.HP, hero.Stats, hero.Equipment)
+}
+
+// An Item is collected and may be used or equipped by the Hero.
+type Item struct {
+	ID          string
+	Name        string
+	Description string
+}
+
+func (item Item) String() string {
+	return fmt.Sprintf("[%v]", item.Name)
+}
+
+// Equipment is a collection of Item objects
+type Equipment map[string]Item
+
+func (equipment Equipment) String() string {
+	var sb strings.Builder
+	for _, item := range equipment {
+		sb.WriteString(item.String())
+	}
+	return fmt.Sprintf("Equipment: {%v}", sb.String())
+}
+
+// A Stat is a number assoicated with a Hero.
+// Stats can be increased, decreased, and checked.
+type Stat struct {
+	ID          string
+	Name        string
+	Description string
+	Value       int
+}
+
+func (stat Stat) String() string {
+	return fmt.Sprintf("(%v: %v)", stat.Name, stat.Value)
+}
+
+// Stats is a collection of Stat objects
+type Stats map[string]Stat
+
+func (stats Stats) String() string {
+	var sb strings.Builder
+	for _, stat := range stats {
+		sb.WriteString(stat.String())
+	}
+	return fmt.Sprintf("Stats: {%v}", sb.String())
+}
 
 // Hello returns a hello message for the provided name.
 func Hello(name string) string {
--- main.go
+++ main.go
@@ -2,11 +2,55 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/urbanyeti/go-hero-game/hero"
 )
 
+type game struct {
+	hero *hero.Hero
+	loop int
+	turn int
+}
+
+func (game game) String() string {
+	return fmt.Sprintf("Loop: %v Turn: %v | %v", game.loop, game.turn, game.hero)
+}
+
 func main() {
-	message := hero.Hello("James")
-	fmt.Printf(message)
+	hero := hero.Hero{
+		ID:          "hero-dan",
+		Name:        "Dan",
+		Description: "a cool hero",
+		HP: hero.Stat{
+			ID:          "stat-hp",
+			Name:        "HP",
+			Description: "health points",
+			Value:       100},
+		Stats: hero.Stats{
+			"stat-atk": hero.Stat{
+				ID:          "stat-atk",
+				Name:        "ATK",
+				Description: "attack stat",
+				Value:       5},
+			"stat-def": hero.Stat{
+				ID:          "stat-def",
+				Name:        "DEF",
+				Description: "defense stat",
+				Value:       5},
+		},
+		Equipment: hero.Equipment{
+			"item-sword1": hero.Item{
+				ID:          "item-sword1",
+				Name:        "Short Sword",
+				Description: "a beginner's basic short sword"},
+			"item-armor1": hero.Item{
+				ID:          "item-armor1",
+				Name:        "Basic Armor",
+				Description: "a beginner's basic set of armor"},
+		}}
+
+	game := game{hero: &hero, loop: 1, turn: 1}
+	fmt.Println(game)
+	time.Sleep((1000))
 }
