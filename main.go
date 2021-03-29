7c8f5ec785cd491ee56b86a908b29630f8c4a2af
Daniel Urbanski <urbanski@gmail.com>
2021-03-29 00:40:46 -0700

    Log to file

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/urbanyeti/go-hero-game/game"
 )
 
 const loopTurns int = 10
 const maxTurns int = 100
 const messageDelay = 0
-const turnDelay = 200
-const loopDelay = 1000
+const turnDelay = 0
+const loopDelay = 0
+
+func setLogging() {
+	var filename string = "logfile.log"
+	// Create the log file if doesn't exist. And append to it if it already exists.
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	Formatter := new(log.TextFormatter)
+
+	// You can change the Timestamp format. But you have to use the same date and time.
+	// "2006-02-02 15:04:06" Works. If you change any digit, it won't work
+	// ie "Mon Jan 2 15:04:05 MST 2006" is the reference time. You can't change it
+	Formatter.TimestampFormat = "02-01-2006 15:04:05"
+	Formatter.FullTimestamp = true
+	log.SetFormatter(Formatter)
+	if err != nil {
+		// Cannot open log file. Logging to stderr
+		fmt.Println(err)
+	} else {
+		log.SetOutput(f)
+	}
+}
 
 func main() {
+	setLogging()
 	game := game.Game{}
 	game.Initialize()
 
